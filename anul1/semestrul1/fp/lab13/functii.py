9ceefa434c76a29186f5db010c1e92d172769db7
vdiana16 <dianavalentinapasca@gmail.com>
2024-09-09 17:49:23 +0300

    Update functii.py

--- anul1/semestrul1/fp/lab13/functii.py
+++ anul1/semestrul1/fp/lab13/functii.py
@@ -1,1 +1,36 @@
+def cresc(lista):
+    if not lista:
+        return False
+    copie = sorted(lista)
+    if copie == lista:
+        return True
+    return False
 
+
+def descresc(lista):
+    if not lista:
+        return False
+    copie = sorted(lista, reverse=True)
+    if copie == lista:
+        return True
+    return False
+
+def conditie(lista, minim):
+    indexminim = lista.index(minim)
+    if indexminim == 0 or indexminim == len(lista) - 1:
+        return False
+    if descresc(lista[:indexminim]) == False or cresc(lista[indexminim + 1:]) == False:
+        return False
+    return True
+
+def printlista(lista):
+    if lista:
+        for el in lista:
+            for i in range(0, len(el)):
+                if i == len(el) - 1:
+                    print(el[i])
+                else:
+                    print(el[i], end=" ")
+    else:
+        raise ValueError("Nu exista permutari!")
+    
